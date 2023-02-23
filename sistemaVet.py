abdb1a4330ea55d8eb569f1cefd9a5d2536e7aa4
duvanVC <duvan.vegac@udea.edu.co>
2023-02-23 13:24:34 -0500

    Ver2

--- sistemaVet.py
+++ sistemaVet.py
@@ -1,94 +1,106 @@
 class Mascota:
-    
+
     def __init__(self):
-        self.__nombre= " "
-        self.__historia=0
-        self.__tipo=" "
-        self.__peso=" "
-        self.__fecha_ingreso=" "
-        self.__medicamento=""
+        self.__nombre = " "
+        self.__historia = 0
+        self.__tipo = " "
+        self.__peso = " "
+        self.__fecha_ingreso = " "
+        self.__medicamento = ""
 
     def verNombre(self):
         return self.__nombre
+
     def verHistoria(self):
         return self.__historia
+
     def verTipo(self):
         return self.__tipo
+
     def verPeso(self):
         return self.__peso
+
     def verFecha(self):
         return self.__fecha_ingreso
+
     def ver_Medicamento(self):
-        return self.__medicamento 
-            
-    def asignarNombre(self,n):
-        self.__nombre=n
-    def asignarHistoria(self,nh):
-        self.__historia=nh
-    def asignarTipo(self,t):
-        self.__tipo=t
-    def asignarPeso(self,p):
-        self.__peso=p
-    def asignarFecha(self,f):
-        self.__fecha_ingreso=f
-    def asignarMedicamento(self,n):
-        self.__medicamento = n 
+        return self.__medicamento
+
+    def asignarNombre(self, n):
+        self.__nombre = n
+
+    def asignarHistoria(self, nh):
+        self.__historia = nh
+
+    def asignarTipo(self, t):
+        self.__tipo = t
+
+    def asignarPeso(self, p):
+        self.__peso = p
+
+    def asignarFecha(self, f):
+        self.__fecha_ingreso = f
+
+    def asignarMedicamento(self, n):
+        self.__medicamento = n
 
 
 class sistemaV:
     def __init__(self):
-        self.__lista_mascotas = []
-        # self.__lista_mascotas = {}
+        self.__lista_felinos = {}
+        self.__lista_caninos = {}
 
-    def verificarExiste(self,historia):
-        for m in self.__lista_mascotas:
-            if historia == m.verHistoria():
-                return True
-        #solo luego de haber recorrido todo el ciclo se retorna False
+    def verificarExiste(self, historia):
+        if historia in self.__lista_felinos:
+            return True
+        if historia in self.__lista_caninos:
+            return True
+        # solo luego de haber recorrido todo el ciclo se retorna False
         return False
 
     def verNumeroMascotas(self):
-        return len(self.__lista_mascotas) 
+        return len(self.__lista_felinos) + len(self.__lista_caninos)
 
-    def ingresarMascota(self,mascota):
-        self.__lista_mascotas.append(mascota) 
-        # self.__lista_mascotas[mascota.verHistoria()]=mascota
+    def ingresarMascota(self, mascota):
+        self.__lista_mascotas.append(mascota)
+        self.__lista_mascotas[mascota.verHistoria()] = mascota
 
-    def verFechaIngreso(self,historia):
-        #busco la mascota y devuelvo el atributo solicitado
+    def verFechaIngreso(self, historia):
+        # busco la mascota y devuelvo el atributo solicitado
         for masc in self.__lista_mascotas:
             if historia == masc.verHistoria():
-                return masc.verFecha() 
+                return masc.verFecha()
         return None
 
-    def verMedicamento(self,historia):
-        #busco la mascota y devuelvo el atributo solicitado
+    def verMedicamento(self, historia):
+        # busco la mascota y devuelvo el atributo solicitado
         for masc in self.__lista_mascotas:
             if historia == masc.verHistoria():
-                return masc.ver_Medicamento() 
+                return masc.ver_Medicamento()
         return None
 
     def eliminarMascota(self, historia):
         for masc in self.__lista_mascotas:
             if historia == masc.verHistoria():
                 # del self.__lista_mascotas[masc]
-                self.__lista_mascotas.remove(masc)  #opcion con el pop
-                return True  #eliminado con exito
-        return False 
+                self.__lista_mascotas.remove(masc)  # opcion con el pop
+                return True  # eliminado con exito
+        return False
 
 
 class Medicamento:
     def __init__(self):
-        self.__nombre = "" 
-        self.__dosis = 0 
-    
+        self.__nombre = ""
+        self.__dosis = 0
+
     def verNombre(self):
-        return self.__nombre 
+        return self.__nombre
+
     def verDosis(self):
-        return self.__dosis 
-    
-    def asignarNombre(self,med):
-        self.__nombre = med 
-    def asignarDosis(self,med):
-        self.__dosis = med 
-        
+        return self.__dosis
+
+    def asignarNombre(self, med):
+        self.__nombre = med
+
+    def asignarDosis(self, med):
+        self.__dosis = med
